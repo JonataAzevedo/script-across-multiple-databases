7f79df03d7854046df46d1c30398318a0f978ca0
jonata.silva <jonata.silva@izatta.tech>
2024-12-11 12:46:06 -0300

    Criado script para rodar um sql em varias bases diferentes

--- sql.js
+++ sql.js
@@ -3,19 +3,7 @@ const fs = require('fs');
 
 const configFile = './db.config.json';
 
-const scriptSql = `
-DO $$
-	DECLARE
-	   qtd INT = 0;
-	begin
-	  	SELECT count(*) INTO qtd FROM autenticacao.t_usuario WHERE email = 'reginaldo.barbara@izatta.tech';
-		IF qtd <= 0 THEN
-			INSERT INTO autenticacao.t_usuario (nome, email, login, valor_senha, reset_token_senha, tentativas_erro_senha, data_ultima_alteracao_senha, status, id_usuario_atualizacao, data_atualizacao, "version")
-			VALUES('Reginaldo Barbara', 'reginaldo.barbara@izatta.tech', 'reginaldo.barbara', null, 'reset-token$2020-reginaldo.barbara', 0, null, 'PENDENTE'::character varying, 0, null, 0);
-			INSERT INTO autenticacao.t_usuario_grupo (id_usuario, id_grupo) 
-			VALUES ((SELECT id FROM autenticacao.t_usuario WHERE login = 'reginaldo.barbara'), 5);
-		END IF;
-	END $$;
+const scriptSql = `sql;
 `;
 
 (async () => {
